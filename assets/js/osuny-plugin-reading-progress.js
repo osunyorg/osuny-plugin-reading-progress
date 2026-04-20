ef7ef63dbedbc71f8059b10a3649ded09c0a3830
Alexis BENOIT <alex@noesya.coop>
2026-04-20 21:54:31 +0200

    script

--- assets/js/osuny-plugin-reading-progress.js
+++ assets/js/osuny-plugin-reading-progress.js
@@ -3,6 +3,7 @@ window.osuny = window.osuny || {};
 window.osuny.PluginReadingProgress = function (element) {
     this.element = element;
     this.bar = this.element.querySelector('.osuny-reading-progress__bar');
+    this.pageFooter = document.querySelector('footer#document-footer');
     this.listen();
 };
 
@@ -13,8 +14,9 @@ window.osuny.PluginReadingProgress.prototype.listen = function () {
 
 window.osuny.PluginReadingProgress.prototype.update = function () {
     var page = document.documentElement,
-        footerHeight = document.querySelector('footer#document-footer').offsetHeight,
-        progression = page.scrollTop / Math.max(0, (page.scrollHeight - footerHeight) - page.clientHeight);
+        scroll = window.pageYOffset || page.scrollTop,
+        footerHeight = this.pageFooter ? this.pageFooter.offsetHeight : 0,
+        progression = scroll / Math.max(0, (page.scrollHeight - footerHeight) - page.clientHeight);
 
     this.bar.style.width = progression * 100 + "%";
 };
